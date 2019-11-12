a0610e8e2e1cf0d43feaf678a3bee709c9dab959
shivaquest <51992666+shivaquest@users.noreply.github.com>
2019-11-12 22:17:24 +0530

    Update SampleController.java

--- src/main/java/com/bt/contoller/SampleController.java
+++ src/main/java/com/bt/contoller/SampleController.java
@@ -20,7 +20,7 @@ public class SampleController {
             throws JSONException {
 System.out.println(":::MEthod calling:::");
         JSONObject js = new JSONObject();
-        js.put("Name", "Bhaskar Reddy L");
+        js.put("Name", "Siva Sankar");
         js.put("Technology", "DevOps  ");
 
         return js.toString();
